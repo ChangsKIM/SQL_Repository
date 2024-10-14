3286bf43f7ecaf98492ad0f592c760ecc98c6b33
ChangsKIM <kcs7601@gmail.com>
2024-10-14 16:52:26 +0900

    함수수업

--- 20241014.sql
+++ 20241014.sql
@@ -9,6 +9,10 @@ SELECT SYSDATE FROM DUAL;
 -- INITCAP : 각 단어별 첫글자는 대문자로 변환, 나머지 글자는 소문자로 변환
 SELECT INITCAP('HELL WORD') FROM DUAL;
 SELECT INITCAP('hell word') FROM DUAL;
--- LOWER :
--- UPPER :
+-- LOWER : 알파벳을 전부 소문자로 변환
+-- UPPER : 알파벳을 전부 대문자로 변환
 SELECT LOWER('Hello World'), UPPER('Hello World') FROM DUAL;  
+-- LENGTH :
+SELECT LENGTH('Hollo'), LENGTH ('안녕하세요') FROM DUAL;
+-- LENGTHB :
+SELECT LENGTHB('Hollo'), LENGTHB('안녕하세요') FROM DUAL;
