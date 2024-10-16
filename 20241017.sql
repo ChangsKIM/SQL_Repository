ee6657242f11d86431ada3120d6b625a7da2cd24
ChangsKIM <kcs7601@gmail.com>
2024-10-16 16:06:51 +0900

    join ; inner join

---
+++ 20241017.sql
@@ -0,0 +1,44 @@
+-- 조인
+-- inner join : 두 테이블에서 조건이 일치하는 행만 반환
+-- 동일, 자연, 크로스 조인이 있음
+CREATE TABLE A(
+    CODE CHAR(1),
+    VAL NUMBER(1)
+);
+CREATE TABLE B(
+    CODE CHAR(1),
+    UNIT CHAR(1)
+);
+INSERT INTO A VALUES('A',1);
+INSERT INTO A VALUES('B',2);
+INSERT INTO A VALUES('C',3);
+INSERT INTO A VALUES('D',4);
+
+INSERT INTO B VALUES('A','+');
+INSERT INTO B VALUES('B','-');
+INSERT INTO B VALUES('C','*');
+INSERT INTO B VALUES('F','/');
+
+SELECT * FROM A;
+SELECT * FROM B;
+
+-- 동일 조인
+-- 방법01
+SELECT A.CODE, A.VAL, B.CODE, B.UNIT 
+FROM A, B
+WHERE A.CODE = B.CODE;
+-- 방법02 <선호01>
+SELECT A.CODE, A.VAL, B.CODE, B.UNIT 
+FROM A INNER JOIN B
+ON A.CODE = B.CODE;
+-- 방법03 <선호02>
+SELECT A.CODE, A.VAL, B.CODE, B.UNIT 
+FROM A JOIN B
+ON A.CODE = B.CODE;
+
+--
+
+
+
+
+
